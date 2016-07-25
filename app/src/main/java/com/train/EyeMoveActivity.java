dfc7dace2c147b40e73e1de7c83c45aed2129e2d
陈秋逸 <qiuyi.chen@criwell.com>
2016-07-25 16:09:20 +0800

    add EyeMoveActivity

---
+++ app/src/main/java/com/train/EyeMoveActivity.java
@@ -0,0 +1,13 @@
+package com.train;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EyeMoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eye_move);
+    }
+}
